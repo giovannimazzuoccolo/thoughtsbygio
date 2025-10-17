111544fcd35a2ed5897ad2f34057c8864e40e867
giovannimazzuoccolo <gio.mazzuoccolo@gmail.com>
2025-10-17 20:05:43 +0200

    Add base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://thoughts.codebygio.me',
+	base: '/thoughtsbygio',
 	integrations: [mdx(), sitemap()],
 });
