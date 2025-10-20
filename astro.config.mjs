073ac8e592ea906667252e535cfeb0d11ed93f9d
giovannimazzuoccolo <gio.mazzuoccolo@gmail.com>
2025-10-20 12:55:16 +0200

    Fix base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://thoughts.codebygio.me',
-	base: '/thoughtsbygio',
+	site: 'https://thoughts.codebyg.io',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
